18c6dad12c8d11f60f274fc29e906af0f1fa760a
Mochammad Masbuchin <buchin@dropsugar.com>
2021-09-01 08:47:06 +0700

    Feature: image filters

--- spec/Scraper.spec.php
+++ spec/Scraper.spec.php
@@ -5,9 +5,24 @@ describe("Scraper", function () {
     describe("scrapeImages", function () {
         it("scrape images from duckduckgo", function () {
             $scraper = new Scraper();
-            $images = $scraper->scrapeImages("makan nasi");
+            $images = $scraper->scrapeImages("makan nasi padang");
 
             expect(count($images) > 0)->toBe(true);
+        });
+
+        it("able to specify several filters", function () {
+            $scraper = new Scraper([
+                "filters" => [
+                    "size" => "Wallpaper",
+                    "color" => "color",
+                    "type" => "transparent",
+                ],
+            ]);
+
+            $images = $scraper->scrapeImages("tokyo revengers");
+            shuffle($images);
+
+            expect($images[0]["width"])->toBeGreaterThan(1000);
         });
     });
 });
--- src/Scraper.php
+++ src/Scraper.php
@@ -1,12 +1,16 @@
 <?php
 namespace Buchin\DdgImages;
 
+use Illuminate\Support\Facades\Http;
+
 class Scraper
 {
     public $base_url = "http://duckduckgo.com/";
     public $options = [
         "max_retry" => 3,
         "proxy" => false,
+        "start" => 0,
+        "filters" => [],
     ];
 
     public function __construct($options = [])
@@ -16,8 +20,10 @@ class Scraper
 
     public function http()
     {
-        $http = new \Illuminate\Http\Client\Factory();
-        return $http;
+        return Http::withHeaders([
+            "User-Agent" =>
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0",
+        ]);
     }
 
     public function getToken($query)
@@ -32,9 +38,7 @@ class Scraper
                 "ia" => "images",
             ]);
 
-        $html = $this->http()
-            ->get($url)
-            ->body();
+        $html = file_get_contents($url);
 
         // vqd='3-142216309694420347760037715005911496568-220342727435306949816443430106535095457';
         $vqd_token = "";
@@ -51,14 +55,22 @@ class Scraper
 
     public function getImages($query)
     {
+        $options = $this->options;
+
+        $filters = $this->buildFilters();
+
         $vqd_token = $this->getToken($query);
+
         $url =
             $this->base_url .
             "i.js?l=wt-wt&o=json&q=" .
             urlencode($query) .
             "&vqd=" .
             $vqd_token .
-            "&f=,,,&p=1&v7exp=a&sltexp=b";
+            "&f=" .
+            $filters .
+            "&p=1&v7exp=a&sltexp=b&s=" .
+            $options["start"];
 
         $html = file_get_contents($url);
         // "results":[{"title":"Nintendo 64 controller
@@ -77,6 +89,30 @@ class Scraper
         }
 
         return $images;
+    }
+
+    public function buildFilters()
+    {
+        // time:Week,size:Wallpaper,color:color,type:transparent,layout:Tall,license:Share
+
+        $filters = "{time},{size},{color},{type},{layout},{license}";
+
+        foreach (
+            ["time", "size", "color", "type", "layout", "license"]
+            as $filter
+        ) {
+            if (!isset($this->options["filters"][$filter])) {
+                $filters = str_replace("{" . $filter . "}", "", $filters);
+            } else {
+                $filters = str_replace(
+                    "{" . $filter . "}",
+                    $filter . ":" . $this->options["filters"][$filter],
+                    $filters
+                );
+            }
+        }
+
+        return $filters;
     }
 
     public function scrapeImages($keyword, $tries = 0)
